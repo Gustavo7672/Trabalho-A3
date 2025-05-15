01a7b04040421ec7ea0cc8d9ed36062d6ac13801
Gustavo Oliveira <go294818@gmail.com>
2025-05-14 21:00:44 -0300

    commit inicial

---
+++ src/Sistema.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Sistema {
+    private Scanner imput = new Scanner(System.in);
+
+    public void menu(){
+
+    }
+
+    public void cadastrar(){
+
+    }
+
+    public void login(){
+
+    }
+
+    public void cadastrarNota(){
+
+    }
+
+    public void cadastrarListaDePresenca() {
+
+
+    }
+}
