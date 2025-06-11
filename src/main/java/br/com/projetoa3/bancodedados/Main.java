667674164bba536000c1e8218eb4e47f1ee67afa
TARSIS45 <tarsisdavi12@gmail.com>
2025-06-11 01:24:26 -0300

    criaçao da tabela alunos do banco de dados

--- src/main/java/br/com/projetoa3/bancodedados/Main.java
+++ src/main/java/br/com/projetoa3/bancodedados/Main.java
@@ -56,5 +56,26 @@ public class Main {
         manager.atualizarNotas();
 
         manager.excluirNotas();
+
+
+        alunosCrud manager = new alunosCrud();
+
+        manager.criarTabelaAlunos();
+
+        manager.inserirAluno(1001, "João da Silva", null, 1, 2);
+
+        manager.listarAlunos();
+
+
+        manager.buscarAlunoPorRA(1001);
+
+
+        manager.buscarAlunoPorNome("João");
+
+
+        manager.atualizarAluno(1001, "João da Silva Atualizado", null, 1, 3);
+
+
+        manager.excluirAluno(1001);
     }
 }
